2134352d7084f0f3f87ffecf495f455fa3d9946e
Masakichi-26 <perrygoro@gmail.com>
2019-10-17 06:07:52 +0000

    first commit

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,14 @@
+class StocksController < ApplicationController
+  
+  def search
+    if params[:stock]
+      @stock ||= Stock.new_from_lookup(params[:stock])
+    end
+    if @stock
+      @stock.can_be_added = current_user.can_add_stock?(@stock.ticker)
+      render json: @stock, methods: [:can_be_added]
+    else
+      render status: 404, json: { response: 'No stock exists for this symbol.' }
+    end
+  end
+end
